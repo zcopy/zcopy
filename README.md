ac8b9963e7068b70193e7d710fee0551373edf32
liuxfe <1206111783@qq.com>
2021-06-17 21:27:11 +0800

    try

--- README.md
+++ README.md
@@ -2,16 +2,3 @@
 permalink: /about.html
 layout: about
 ---
-
-#### 关于网站
-
---- 
-
-> 这个网站是 [webstack.cc](https://github.com/WebStackPage/WebStackPage.github.io) 的 [Jekyll](https://jekyllrb.com/) 版。
-
-
-# ➡️ [webstack.0xl2oot.cn](https://webstack.0xl2oot.cn/) - 网址导航
-
-
-> 这是一个纯静态的网址导航网站，内容均由 [viggo](http://viggoz.com/) 收集并整理。项目基于bootstrap前端框架开发。
-
---
+++ main.js
@@ -0,0 +1,29 @@
+// Google AdSense
+(function(){
+  var ad = document.createElement("script");
+  ad.setAttribute("data-ad-client", "ca-pub-9312750344484857");
+  ad.setAttribute("async", true);
+  ad.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+  document.head.appendChild(ad);
+})();
+
+// Google Analytics
+(function(){
+  var ga = document.createElement("script");
+  ga.setAttribute("async", true);
+  ga.src = "https://www.googletagmanager.com/gtag/js?id=UA-195431371-1";
+  document.head.appendChild(ga);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-195431371-1');
+})();
+
+// baidu tongji
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bc24c40486ac78730f85549d164dac6f";
+  document.head.appendChild(hm);
+})();
