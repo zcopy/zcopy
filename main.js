db9dde9d78f4358557f3c8721c77f894a440de9e
liuxfe <1206111783@qq.com>
2023-04-10 22:16:15 +0800

    fixs

--- main.js
+++ main.js
@@ -21,10 +21,16 @@
   gtag('config', 'UA-195431371-1');
 })();
 */
-// baidu tongji
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?358b1a485f5d2756a663674529013da2";
-  document.head.appendChild(hm);
-})();
+
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(93154105, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+
